9b9254ec0ad9d52da391e9f81f2429d9d8a860af
Yasar962 <118204132+Yasar962@users.noreply.github.com>
2025-12-28 19:50:33 +0530

    Add repo router to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.auth.github import router as github_router
+from app.api.repo import router as repo_router
 
 load_dotenv(".env.local")
 
 app = FastAPI(title="Octopus Backend")
 
 app.include_router(github_router)
+app.include_router(repo_router)
 
 @app.get("/")
 def root():
